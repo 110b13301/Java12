4d4850ef6d454aab69c27e316ada85f31aedcfb7
110b13301 <112999887+110b13301@users.noreply.github.com>
2022-12-20 10:52:35 +0800

    Delete Java15 directory

--- Java15/Java15.java
+++
@@ -1,6 +0,0 @@
-public class Java15 {
-	public static void main(String[] args) {
-		String s=Double.toString(123.45); // convert double to string
-		System.out.println(s.length()); // print the length of string
-	}
-}
